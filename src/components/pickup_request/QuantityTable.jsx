b40ad59e230bcef8b0c6b8ec22ba06f9a684f680
Pando India Software Consultants <pandoemp0107@pandoconsultants.com>
2023-05-29 15:03:12 +0530

    pickup request corrected

--- src/components/pickup_request/QuantityTable.jsx
+++ src/components/pickup_request/QuantityTable.jsx
@@ -1,19 +1,24 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { useState } from 'react';
-// import { initialPickupState } from '../../redux/features/pickupSlice';
 
-function QuantityTable({ selectedCheckboxes, onFormChange }) {
+function QuantityTable({ selectedCheckboxes }) {
   const selectedItems = selectedCheckboxes;
-  const [formData, setFormData] = useState([]);
+  const [formData, setFormData] = useState({
+    pickup_request_items: [],
+  });
 
-  // const handleQuantityChange = (itemId, quantity) => {
-  //   const updatedFormData = formData.filter((item) => item.id !== itemId); // Remove the item if it already exists
-  //   updatedFormData.push({ id: itemId, quantity }); // Add the item with the updated quantity
-  //   setFormData(updatedFormData);
-  //   props.onFormChange({ pickup_request_items: updatedFormData }); // Pass the updated form data to the parent component
-  // };
   const handleQuantityChange = (itemId, quantity) => {
-    onFormChange(itemId, quantity);
+    const updatedFormData = formData.pickup_request_items.map((item) => {
+      if (item.item_id === itemId) {
+        return { ...item, weight: quantity };
+      }
+      return item;
+    });
+
+    setFormData((prevState) => ({
+      ...prevState,
+      pickup_request_items: updatedFormData,
+    }));
   };
 
   return (
@@ -26,25 +31,30 @@ function QuantityTable({ selectedCheckboxes, onFormChange }) {
         </tr>
       </thead>
       <tbody className='w-full'>
-        {selectedItems?.map((checkedValue) => (
-          <tr key={checkedValue.id}>
-            <td className='text-center'>{checkedValue.item_name}</td>
-            <td className='text-center'>{checkedValue.rate}</td>
-            <td>
-              <select
-                className='block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500'
-                name='quantity'
-                value={formData.find((item) => item.id === checkedValue.id)?.quantity || ''}
-                onChange={(e) => handleQuantityChange(checkedValue.id, e.target.value)}
-              >
-                <option value=''>Select Quantity</option>
-                <option value='<500g'>less than 500g</option>
-                <option value='500g-5kg'>500g-5kg</option>
-                <option value='>5kg'>greater than 5kg</option>
-              </select>
-            </td>
-          </tr>
-        ))}
+        {selectedItems?.map((checkedValue) => {
+          const selectedItem = formData.pickup_request_items.find((item) => item.id === checkedValue.id);
+          const selectedQuantity = selectedItem ? selectedItem.quantity : '';
+
+          return (
+            <tr key={checkedValue.id}>
+              <td className='text-center'>{checkedValue.item_name}</td>
+              <td className='text-center'>{checkedValue.rate}</td>
+              <td>
+                <select
+                  className='block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500'
+                  name='quantity'
+                  defaultValue={selectedQuantity} // Use defaultValue instead of value
+                  onChange={(e) => handleQuantityChange(checkedValue.id, e.target.value)}
+                >
+                  <option value=''>Select Quantity</option>
+                  <option value='<500g'>less than 500g</option>
+                  <option value='500g-5kg'>500g-5kg</option>
+                  <option value='>5kg'>greater than 5kg</option>
+                </select>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
--- src/redux/features/pickupSlice.js
+++ src/redux/features/pickupSlice.js
@@ -1,8 +1,9 @@
-import { createSlice, createAsyncThunk, applyMiddleware } from '@reduxjs/toolkit';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import * as api from '../api';
 
 export const initialPickupState = {
-  user: '',
   pickup_date: '',
   pickup_time: '',
   flat_number: '',
@@ -11,9 +12,41 @@ export const initialPickupState = {
   city: '',
   state: '',
   pincode: '',
-  pickup_request_items: {
-    item_rate: '',
-    weight: '',
-  },
+  pickup_request_items: [],
 };
-export const orderPickup = createAsyncThunk('pickup');
+
+export const orderPickup = createAsyncThunk('pickup/orderPickup', async (formData, { rejectWithValue }) => {
+  try {
+    const authToken = localStorage.getItem('AuthToken'); // Retrieve the auth token from localStorage
+    const response = await api.pickupRequest(formData, authToken); // Pass the authToken to the API request
+    return response?.data; // Assuming the response contains the desired data
+  } catch (e) {
+    return rejectWithValue(e?.response?.data); // Assuming the error response contains the desired error message
+  }
+});
+
+const pickupSlice = createSlice({
+  name: 'pickup',
+  initialState: {
+    data: null,
+    error: '',
+    loading: false,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(orderPickup.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(orderPickup.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action?.payload;
+      })
+      .addCase(orderPickup.rejected, (state, action) => {
+        state.loading = false;
+        state.data = { ...action.payload, headers: undefined };
+      });
+  },
+});
+
+export default pickupSlice.reducer;
