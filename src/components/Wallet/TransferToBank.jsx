019130dbb6f120a57a4935ef5f4c158adaaeebfb
dixitsahil <dixitsahil0@gmail.com>
2023-08-09 18:51:35 +0530

    validation bug fixed

--- src/components/Wallet/TransferToBank.jsx
+++ src/components/Wallet/TransferToBank.jsx
@@ -42,8 +42,10 @@ function TransferToBank() {
 
   const validateForm = (data) => {
     const errors = {};
-    if (!data.account_number) {
-      errors.account_number = 'Account Number is required';
+    if (!/^[0-9]+$/.test(data.account_number)) {
+      errors.account_number = 'Account Number must contain only numbers';
+    } else if (data.account_number.length < 9 || data.account_number.length > 18) {
+      errors.account_number = 'Account Number must be between 9 and 18 digits';
     }
     else if (!/^[0-9]+$/.test(data.account_number)) {
       errors.account_number = 'Account Number must contain only numbers';
@@ -51,19 +53,66 @@ function TransferToBank() {
 
     if (!data.ifsc_code) {
       errors.ifsc_code = 'IFSC Code is required';
+    } else if (!/^[A-Za-z0-9]+$/.test(data.ifsc_code)) {
+      errors.ifsc_code = 'IFSC Code should only contain letters and numbers';
     }
 
     if (!data.branch_name) {
       errors.branch_name = 'Branch Name is required';
     }
 
+    const trimmedBranchName = data.branch_name.trim();
+
+    if (!trimmedBranchName) {
+      errors.branch_name = 'Branch Name is required';
+    }
+    else {
+      const hasNumbers = /\d/.test(trimmedBranchName);
+      const hasSpecialCharacters = /[^a-zA-Z\s]/.test(trimmedBranchName);
+
+      if (hasNumbers) {
+        errors.branch_name = 'Branch Name cannot contain numbers';
+      } else if (hasSpecialCharacters) {
+        errors.branch_name = 'Branch Name cannot contain special characters';
+      }
+    }
+
     if (!data.bank_name) {
       errors.bank_name = 'Bank Name is required';
     }
 
-    if (!data.account_holder_name) {
+    const trimmedBankName = data.bank_name.trim();
+
+    if (!trimmedBankName) {
+      errors.bank_name = 'Bank Name is required';
+    }
+    else {
+      const hasNumbers = /\d/.test(trimmedBankName);
+      const hasSpecialCharacters = /[^a-zA-Z\s]/.test(trimmedBankName);
+
+      if (hasNumbers) {
+        errors.bank_name = 'Bank Name cannot contain numbers';
+      } else if (hasSpecialCharacters) {
+        errors.bank_name = 'Bank Name cannot contain special characters';
+      }
+    }
+
+    const trimmedAccountHolderName = data.account_holder_name.trim();
+
+    if (!trimmedAccountHolderName) {
       errors.account_holder_name = 'Account Holder Name is required';
     }
+    else {
+      const hasNumbers = /\d/.test(trimmedAccountHolderName);
+      const hasSpecialCharacters = /[^a-zA-Z\s]/.test(trimmedAccountHolderName);
+
+      if (hasNumbers) {
+        errors.account_holder_name = 'Account Holder Name cannot contain numbers';
+      } else if (hasSpecialCharacters) {
+        errors.account_holder_name = 'Account Holder Name cannot contain special characters';
+      }
+    }
+
 
     return errors;
   };
@@ -90,13 +139,13 @@ function TransferToBank() {
             <form onSubmit={handleSubmit} action="" className="pt-10 text-xl">
               <h1 className='lg:mt-10 md:mt-10 sm:mt-10 mt-20 text-start lg:text-4xl md:test-4xl sm:text-4xl text-2xl font-medium text-[#263238]'>Transfer to your bank account</h1>
               <div className='grid grid-cols-1 sm:grid-cols-2 gap-8 mt-10'>
-              <div className='text-start'>
+                <div className='text-start'>
                   <label htmlFor="phone" className='text-[#000000bf] font-medium'>Account Holder Name *</label>
                   <br />
                   <input type="text" className={`h-16 w-full bg-gray-200 border-2 rounded-xl p-5 mt-3 ${formErrors.account_holder_name ? 'border-red-500' : ''
                     }`} name="account_holder_name" value={bankForm?.account_holder_name} placeholder="Enter Account Holder Name"
                     onChange={handleInputChange} /><br />
-                  {formErrors.account_number && (
+                  {formErrors.account_holder_name && (
                     <span className="text-red-500 text-sm font-medium">{formErrors.account_holder_name}</span>
                   )}
                 </div>
@@ -117,7 +166,7 @@ function TransferToBank() {
                   <input type="text" className={`h-16 w-full bg-gray-200 border-2 rounded-xl p-5 mt-3 ${formErrors.bank_name ? 'border-red-500' : ''
                     }`} name="bank_name" placeholder="Enter Bank Name" value={bankForm?.bank_name}
                     onChange={handleInputChange} /><br />
-                  {formErrors.account_number && (
+                  {formErrors.bank_name && (
                     <span className="text-red-500 text-sm font-medium">{formErrors.bank_name}</span>
                   )}
                 </div>
@@ -127,7 +176,7 @@ function TransferToBank() {
                   <input type="text" className={`h-16 w-full bg-gray-200 border-2 rounded-xl p-5 mt-3 ${formErrors.ifsc_code ? 'border-red-500' : ''
                     }`} name="ifsc_code" value={bankForm?.ifsc_code} placeholder="Enter IFSC Code"
                     onChange={handleInputChange} /><br />
-                  {formErrors.account_number && (
+                  {formErrors.ifsc_code && (
                     <span className="text-red-500 text-sm font-medium">{formErrors.ifsc_code}</span>
                   )}
                 </div>
@@ -140,12 +189,12 @@ function TransferToBank() {
                     }`} name="branch_name" value={bankForm?.branch_name} placeholder="Enter Branch Name"
                     onChange={handleInputChange} />
                   <br />
-                  {formErrors.account_number && (
+                  {formErrors.branch_name && (
                     <span className="text-red-500 text-sm font-medium">{formErrors.branch_name}</span>
                   )}
                 </div>
-                
-                
+
+
               </div>
               <div className="flex justify-center pb-20">
                 <button type="submit" className="text-center rounded-2xl lg:text-2xl md:text-2xl text-xl p-5 text-white lg:px-20 md:px-16  bg-[#27AE76] justify-center mt-20">
