0fb37b34db026ed18c7e04185268a98067a67dbb
dixitsahil <dixitsahil0@gmail.com>
2023-08-14 18:42:32 +0530

    wallet history integration

--- src/components/Wallet/TransactionTable.jsx
+++ src/components/Wallet/TransactionTable.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchWalletHistoryData } from '../../redux/features/getWalletHistorySlice';
 
 function TransactionTable(props) {
-  const { name, heading } = props;
+  const { name, heading, walletId, transactionTypeFilter } = props;
+  const dispatch = useDispatch();
+  const tableData = useSelector(state => state.walletHistoryDetails.data?.data || []);
+  
+  useEffect(() => {
+    if (walletId !== null) {
+      const response = dispatch(fetchWalletHistoryData(walletId));
+    }
+  }, [dispatch, walletId]);
+
+  let data = [];
+
+  if (transactionTypeFilter === 'dr') {
+    data = tableData.filter(transaction => transaction.transaction_type === 'dr');
+  } else if (transactionTypeFilter === 'cr') {
+    data = tableData.filter(transaction => transaction.transaction_type === 'cr');
+  }
+
   return (
     <>
       {' '}
@@ -11,7 +30,7 @@ function TransactionTable(props) {
           <hr className='border-1 mb-5'></hr>
           <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
-              <thead className='text-xs text-gray-700 uppercase bg-gray-50 gray:bg-gray-700 dark:text-gray-400'>
+              <thead className='text-sm text-gray-700 uppercase bg-gray-50 gray:bg-gray-700 dark:text-gray-400'>
                 <tr className=''>
                   {heading?.map((head, index) => (
                     <th key={index} scope="col" className="px-6 py-3 text-black" >{head}</th>
@@ -19,15 +38,26 @@ function TransactionTable(props) {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td />
-                  <td />
-                  <td />
-                  <td />
-                  <td />
-                  <td />
+              {data.map((transaction, index) => {
+                const [date, timeAndOffset] = transaction.wallet_updated.split('T');
+                const [, time] = timeAndOffset.split('+');
+                return (
+                <tr key={index} className={`${
+                  index % 2 === 0 ? 'bg-white-100' : 'bg-green-50'
+                } border-b`}>
+                  <td className="px-6 py-4 text-[#000000a6] font-medium">
+                    {transactionTypeFilter === 'dr'
+                      ? index+1
+                      : transaction.order_id
+                    }
+                  </td>
+                  <td className="px-6 py-4 text-[#000000a6] font-medium">{date}</td>
+                  <td className="px-6 py-4 text-[#000000a6] font-medium">{time}</td>
+                  <td className="px-6 py-4 text-[#000000a6] font-medium">{transaction.transaction_amount}</td>
                 </tr>
-              </tbody>
+                )}
+                )}
+            </tbody>
             </table>
           </div>
         </div>
