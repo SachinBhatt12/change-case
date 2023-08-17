a8061dc5f4152dc381a61429d9bf3880ba223868
visparmar119 <138429422+visparmar119@users.noreply.github.com>
2023-08-17 17:25:53 +0530

    add social links

--- src/components/footer/SocialHandle.jsx
+++ src/components/footer/SocialHandle.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   FaLinkedin, FaFacebook, FaTwitter, FaInstagram,
 } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 
 function SocialHandle() {
   return (
@@ -9,18 +10,22 @@ function SocialHandle() {
       <div className='text-sm lg:text-lg'>
         <h1> Follow us on : </h1>
       </div>
-      <div className='ml-2'>
-        <FaLinkedin />
-      </div>
-      <div className='ml-2'>
-        <FaFacebook />
-      </div>
-      <div className='ml-2'>
+      <NavLink to='https://www.linkedin.com/company/pando-india-software-consultants/'>
+        <div className='ml-2 cursor-pointer'>
+          <FaLinkedin />
+        </div>
+      </NavLink>
+      <NavLink to='https://www.facebook.com/people/Pando-India-Software-Consultants/100084361683435/'>
+        <div className='ml-2 cursor-pointer'>
+          <FaFacebook />
+        </div>
+      </NavLink>
+      {/* <div className='ml-2'>
         <FaTwitter />
       </div>
       <div className='ml-2'>
         <FaInstagram />
-      </div>
+      </div> */}
     </div>
   );
 }
